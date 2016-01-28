021916fce6c57323f91e4eaa5d46f49dfdf9a17f
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-27 16:35:48 -0800

    working version for one database

--- app.js
+++ app.js
@@ -8,39 +8,63 @@ const Authenticat = require('authenticat');
 const app = express();
 const router = require('./lib/router');
 
-var p1 = new Promise( (resolve, reject) => {
-  const connection = mongoose.createConnection('mongodb://localhost/tokenAuth');
-  const authenticat = new Authenticat(connection);
+mongoose.connect('mongodb://localhost/tokenAuth');
+const connection = mongoose.connection;
+const authenticat = new Authenticat(connection);
 
-  app.use('/', authenticat.router); //general routing for signup, login and roles
-  app.use('/employees', authenticat.tokenAuth, router);
+app.use('/', authenticat.router); //general routing for signup, login and roles
+app.use('/employees', authenticat.tokenAuth, router);
 
-  connection.on('error', err => {
+//404
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+connection.on('error', err => {
     console.log(err);
     connection.close();
-    reject(err);
   });
-  connection.once('open', () => {
-    console.log('User database connected');
-    resolve();
-  });
-});
-
-var p2 = new Promise( (resolve, reject) => {
-  const connectionToEmployee = mongoose.createConnection('mongodb://localhost/test');
-
-  connectionToEmployee.on('error', err => {
-    console.log(err);
-    connectionToEmployee.close();
-    reject(err);
-  });
-  connectionToEmployee.on('open', () => {
-    console.log('Employee database connected');
-    resolve();
-  });
-});
-
-Promise.all([p1,p2]).then( () => {
+connection.once('open', () => {
+  console.log('User database connected');
   app.listen(3000, console.log('Server has started. Listening at port 3000...'));
 });
+
+// var p1 = new Promise( (resolve, reject) => {
+//   const connection = mongoose.createConnection('mongodb://localhost/tokenAuth');
+//   const authenticat = new Authenticat(connection);
+
+//   app.use('/', authenticat.router); //general routing for signup, login and roles
+//   app.use('/employees', authenticat.tokenAuth, router);
+
+//   connection.on('error', err => {
+//     console.log(err);
+//     connection.close();
+//     reject(err);
+//   });
+//   connection.once('open', () => {
+//     console.log('User database connected');
+//     resolve();
+//   });
+//   connection.on('close', () => {
+//     console.log('connection closed');
+//   });
+// });
+
+// var p2 = new Promise( (resolve, reject) => {
+//   const connectionToEmployee = mongoose.createConnection('mongodb://localhost/test');
+
+//   connectionToEmployee.on('error', err => {
+//     console.log(err);
+//     connectionToEmployee.close();
+//     reject(err);
+//   });
+//   connectionToEmployee.on('open', () => {
+//     console.log('Employee database connected');
+//     resolve();
+//   });
+// });
+
+// Promise.all([p1,p2]).then( () => {
+//   app.listen(3000, console.log('Server has started. Listening at port 3000...'));
+// });
 
